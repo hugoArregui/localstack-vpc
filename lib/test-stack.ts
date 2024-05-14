6337298bf9f4f0ad4c2299666a4a2ade913f9e29
Hugo Arregui <969314+hugoArregui@users.noreply.github.com>
2024-05-14 16:58:18 -0300

    vpc

--- lib/test-stack.ts
+++ lib/test-stack.ts
@@ -1,19 +1,18 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_ec2 } from "aws-cdk-lib";
 
 export class TestStack extends Stack {
+  private vpc: aws_ec2.Vpc;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'TestQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    this.vpc = new aws_ec2.Vpc(this, "vpc");
 
-    const topic = new sns.Topic(this, 'TestTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const subnetIds = [];
+    for (const subnet of this.vpc.privateSubnets) {
+      subnetIds.push(subnet.subnetId);
+    }
   }
 }
